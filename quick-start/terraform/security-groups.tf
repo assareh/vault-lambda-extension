d037096afd2035ee73f0f1c5599aae2d56761961
assareh <assareh@hashicorp.com>
2022-11-29 01:04:50 -0800

    removed 8200 ingress

--- quick-start/terraform/security-groups.tf
+++ quick-start/terraform/security-groups.tf
@@ -17,12 +17,12 @@ resource "aws_security_group" "vault-server" {
   }
 
   # Vault API traffic
-  ingress {
-    from_port   = 8200
-    to_port     = 8200
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 8200
+  #   to_port     = 8200
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   # Vault cluster traffic
   # ingress {
