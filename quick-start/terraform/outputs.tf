2e757e0f847394fd9091e2e317b43ea238313c9f
Andy Assareh <assareh@me.com>
2022-11-28 20:27:23 -0800

    removing lambda

--- quick-start/terraform/outputs.tf
+++ quick-start/terraform/outputs.tf
@@ -7,16 +7,5 @@ Vault UI URL:             http://${aws_instance.vault-server.public_ip}:8200/ui
 You can SSH into the Vault EC2 instance using private.key:
     ssh -i private.key ubuntu@${aws_instance.vault-server.public_ip}
 
-You are now ready to run your Lambda function. Assuming `aws` is configured to
-use the same account that terraform deployed to (configured by the AWS_...
-environment variables), you can run:
-
-aws lambda invoke --function-name ${aws_lambda_function.function.function_name} /dev/null \
-    --cli-binary-format raw-in-base64-out \
-    --log-type Tail \
-    --region ${var.aws_region} \
-    | jq -r '.LogResult' \
-    | base64 --decode
-
 EOF
 }
